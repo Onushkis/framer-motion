4ba69f2d9222e47982c407c6ff4e9ca591d40085
Ona <onushkis@gmail.com>
2022-01-28 11:20:00 +0100

    added clean 2 lesson

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -5,15 +5,14 @@ import { motion } from 'framer-motion';
 const Home = () => {
   return (
     <motion.div className="home container"
-    animate={{ rotateZ:180, opacity: 0.2, margintTop: 200 }}
+    animate={{}}
     >
-      <motion.h2 
-          animate={{ fontSize: 50, color: '#ff2994', x: 100, y: -100  }}>
+      <h2>
         Welcome to Pizza Joint
-      </motion.h2>
+      </h2>
       <Link to="/base">
         <motion.button 
-            animate={{ scale: 1.5 }}>
+            animate={{ }}>
           Create Your Pizza
         </motion.button>
       </Link>
