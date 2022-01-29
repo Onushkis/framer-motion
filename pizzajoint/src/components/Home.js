147a63ea4e68229a0611671f3241b7889aebe55c
Ona <onushkis@gmail.com>
2022-01-29 15:43:25 +0100

    added Keyframes moving btn

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
+
+const buttonVariants = {
+  /*  //left right moving
+  visible : {
+    x: [0, -20, 20, -20, 0],
+transition: {delay: 2 }
+  }, */
+  hover: {
+    //keyframes moving btn
+    scale: [1,1.1,1,1.1,1,1.1,1],
+    textShadow: '0px 0px 8px rgb(255,255,255)',
+    boxShadow: '0px 0px 8px rgb(255,255,255)',
+  },
+}
 
 const Home = () => {
   return (
-    <motion.div className="home container"
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-   transition={{ delay: 1.5, duration: 1.5}}
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      /*   left right moving line 7
+   transition={{ delay: 1.5, duration: 1.5}} */
     >
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button 
-            whileHover={{ 
-              scale: 1.1,
-              textShadow: "0px 0px 8px rgb(255,255,255)",
-              boxShadow: "0px 0px 8px rgb(255,255,255)",
-             }}
-
-            >
+        <motion.button
+          variants={buttonVariants}
+          animate="visible"
+          whileHover="hover"
+        >
           Create Your Pizza
         </motion.button>
       </Link>
@@ -28,4 +39,4 @@ const Home = () => {
   )
 }
 
-export default Home;
+export default Home
